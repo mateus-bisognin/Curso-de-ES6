a8b9dd756c155ad27c92ae6092ea8c892cc9b7c3
Mateus Bisognin <mateus_bisognin@hotmail.com>
2018-08-14 21:21:29 -0300

    Dia 14/08/2018 - Aulas 3 e 4 do Módulo 4

--- src/main - Mod 4 Aula 3 Render em tela.js
+++ src/main - Mod 4 Aula 3 Render em tela.js
@@ -1,13 +1,10 @@
-import api from './api';
-
 class App {
   constructor() {
     this.repositories = [];
 
     this.formEl = document.getElementById('repo-form');
     this.listEl = document.getElementById('repo-list');
-    this.inputEl = document.querySelector('input[name=repository]');
-    
+
     this.registerHandlers();
   }
 
@@ -15,26 +12,15 @@ class App {
     this.formEl.onsubmit = event => this.addRepository(event);
   }
 
-  async addRepository(event) {
+  addRepository(event) {
     event.preventDefault();
 
-    const repoInput = this.inputEl.value;
-
-    if (repoInput.length === 0)
-      return;
-
-    const response = await api.get(`/repos/${repoInput}`);
-
-    const { name, description, html_url, owner: { avatar_url } } = response.data;
-    
     this.repositories.push({
-      name,
-      description,
-      avatar_url,
-      html_url,
+      name: 'rocketseat.com.br',
+      description: 'Tire a sua ideia do papel e dê vida à sua startup',
+      avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+      html_url: 'http://github.com/rocketseat/rocketseat.com.br',
     });
-
-    this.inputEl.value = '';
 
     this.render();
   }
